c91f490b2d6b4d11b9a1bfb136065abd22a06e08
Yaniv Iny <yiny@ucsd.edu>
2025-02-25 11:39:53 -0800

    adding files

---
+++ class4/class4.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "BIMM 143 Class 4"
+#' author: "Yaniv Iny (PIDA18090586)"
+#' date: "January 16, 2025"
+#' ---
+
+
+
+x <- 1:50
+plot(x, sin(x))
+
+# make this nicer 
+plot(x, sin(x), col="pink", type="l",lwd=3)
